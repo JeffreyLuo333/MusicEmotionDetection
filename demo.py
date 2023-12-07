9d467f5740fd80f9314b09eada40da9463fe95d8
Jeffrey Luo <114297879+JeffreyLuo333@users.noreply.github.com>
2023-12-07 02:44:30 -0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,75 @@
+"""
+Author: Jeffrey Luo, Monta Vista High School, Cupertino, CA
+Date Created: 09/2023
+Copyright (c) 2023 Jeff Luo
+License: MIT
+"""
+import torch
+import numpy as np
+import pandas as pd
+
+from model_torch import Audio2EmotionModel
+
+def rescale(scores:np.ndarray) -> np.ndarray:
+    """
+    Rescale the model output back into original range (1 ~ 7.83)
+    
+    Args:
+        scores: the model prediction in [0,1], shape (B, 8)
+
+    Returns:
+        Rescaled model prediction in [1, 7.83], shape (B, 8)
+    """
+    scores = scores * 10
+    for r in range(scores.shape[0]):
+        for s in range(scores.shape[-1]):
+            if scores[r,s] < 1:
+                scores[r,s] = 1
+            elif scores[r,s] > 7.83:
+                scores[r,s] = 7.83
+    
+    return scores
+
+def print_scores(scores: np.ndarray) -> None:
+    """
+    Pretty print for print out scores 
+    """
+    df = pd.DataFrame(scores, columns=['valence', 'energy', 'tension', 
+                                       'anger', 'fear', 'happy', 'sad', 'tender'])
+    print(df)
+
+
+if __name__ == '__main__':
+    # config
+    checkpoint_path = 'weights/best.pth'
+    # test audio spectrograms
+    audio_spectrograms_path = 'data/spectrograms/001.mp3.npy'
+
+    # init model and loading weights
+    model = Audio2EmotionModel()
+    model.load_state_dict(torch.load(checkpoint_path))
+    model.eval()
+    model.cuda()
+
+    # load test audio spectrograms
+    audio_spectrograms = np.load(audio_spectrograms_path)
+    # add batch,chanel dim
+    audio_spectrograms = np.expand_dims(audio_spectrograms, (0, 1))
+
+    audio_spectrograms = torch.tensor(audio_spectrograms)
+
+    # inference
+    with torch.no_grad():
+        audio_spectrograms = audio_spectrograms.to('cuda')
+        pred = model(audio_spectrograms)
+        # offload to cpu
+        pred = pred.cpu().numpy()
+
+        # shape check: if single batch add batch dim
+        if len(pred.shape) == 1:
+            pred = np.expand_dims(pred, 0)
+
+        # rescale
+        pred_rescaled = rescale(pred)
+        # print result
+        print_scores(pred_rescaled)
